adca227921b8cc25dcd27628a7d86be5e0969847
Minso <minso9187@gmail.com>
2026-09-13 02:18:19 +0200

    Added B-Spline Surface

--- GeoKernel3D/BSplineSurface.h
+++ GeoKernel3D/BSplineSurface.h
@@ -11,8 +11,10 @@ public:
 	std::vector<double> KnotsU;
 	std::vector<double> KnotsV;
 
-	std::vector<std::vector<Point3D>> ControlPoints;
+	std::vector<std::vector<Point3D>> ControlNet;
 
 public:
 	Point3D Evaluate(double u, double v) const;
+	double BasisFunction(int i, int degree, double t, const std::vector<double>& knots) const;
+
 };
